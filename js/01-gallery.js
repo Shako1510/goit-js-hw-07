9f0f2037ef72052c57dcfaffbe07385b9dcef9bb
Shako Yevhen <schako15101992@gmail.com>
2022-07-16 20:58:18 +0300

    Створив розмітку за шаблоном

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+console.log(createPatenrnGallery(galleryItems));
+
+const listGallery = document.querySelector('.gallery')
+
+function createPatenrnGallery(galleryItems) {
+
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src='${preview}'
+      data-source='${original}'
+      alt='${description}'/>
+  </a>
+</div>`;
+    }).join('');
+
+}
+
+listGallery.insertAdjacentHTML('beforeend', createPatenrnGallery(galleryItems));
+
+// console.log(createPatenrnGallery(galleryItems));
+
+
+
+
