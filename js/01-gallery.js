53a1b8728b51eb00a6979fdc8c7567d4395a5e5b
Shako Yevhen <schako15101992@gmail.com>
2022-07-16 23:26:13 +0300

    Зробив завд 1, модуль 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-console.log(galleryItems);
-console.log(createPatenrnGallery(galleryItems));
+// console.log(galleryItems);
 
 const listGallery = document.querySelector('.gallery')
 
@@ -25,6 +24,27 @@ listGallery.insertAdjacentHTML('beforeend', createPatenrnGallery(galleryItems));
 
 // console.log(createPatenrnGallery(galleryItems));
 
+listGallery.addEventListener('click', showModal);
+
+function showModal(evt) {
+    evt.preventDefault();
+    if (!evt.target.classList.contains('gallery__image')) {
+        return
+    }
+
+    const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}">
+`)
+
+    instance.show();
+
+
+    listGallery.addEventListener('keydown', (evt) => {
+        if (evt.code === 'Escape') {
+            instance.close();
+        }
+    })
+};
 
 
 
