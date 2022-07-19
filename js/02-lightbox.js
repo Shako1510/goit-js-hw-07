67e4859f19f9e02a5c8829ebb226599c358d477a
Shako Yevhen <schako15101992@gmail.com>
2022-07-19 18:19:08 +0300

    Зробив завд 2 модуль7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,35 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const listGallery = document.querySelector('.gallery')
+
+function createPatenrnGallery(galleryItems) {
+
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src='${preview}'
+      data-source='${original}'
+      alt='${description}'/>
+  </a>
+`;
+    }).join('');
+
+}
+
+listGallery.insertAdjacentHTML('beforeend', createPatenrnGallery(galleryItems));
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionsDelay: 250,
+});
+
+// lightbox.on('show.simplelightbox', function () {
+
+//     const gallery = $('.gallery a').simpleLightbox();
+
+//     gallery.next();
+// });
